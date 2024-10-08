7b88cf365fdd2d945d057ccda20d0c0585bd0be7
Julyano Costa <julyano.99@hotmail.com>
2024-10-08 11:36:37 -0300

    GET products paged

--- src/main/java/com/jcosta/dscommerce/controllers/ProductController.java
+++ src/main/java/com/jcosta/dscommerce/controllers/ProductController.java
@@ -2,16 +2,16 @@ package com.jcosta.dscommerce.controllers;
 
 
 import com.jcosta.dscommerce.dto.ProductDTO;
-import com.jcosta.dscommerce.entities.Product;
-import com.jcosta.dscommerce.repositotries.ProductRepository;
 import com.jcosta.dscommerce.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Optional;
+import java.util.List;
 
 @RestController // esta anotation faz com que o que seja criado nesta classe responda na web
 @RequestMapping(value = "/products")
@@ -24,6 +24,11 @@ public class ProductController {
     @GetMapping(value = "/{id}")
     public ProductDTO findById(@PathVariable Long id) {
         return service.findById(id);
+    }
+
+    @GetMapping
+    public Page<ProductDTO> findAll(Pageable pageable) {
+        return service.findAll(pageable);
     }
 
 }
--- src/main/java/com/jcosta/dscommerce/services/ProductService.java
+++ src/main/java/com/jcosta/dscommerce/services/ProductService.java
@@ -5,9 +5,12 @@ import com.jcosta.dscommerce.dto.ProductDTO;
 import com.jcosta.dscommerce.entities.Product;
 import com.jcosta.dscommerce.repositotries.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -18,10 +21,14 @@ public class ProductService {
 
     @Transactional(readOnly = true)
     public ProductDTO findById(Long id) {
-        Optional<Product> result = repository.findById(id);
-        Product product = result.get();
-        ProductDTO dto = new ProductDTO(product);
-        return dto;
+        Product product = repository.findById(id).get();
+        return new ProductDTO(product);
+    }
+
+    @Transactional(readOnly = true)
+    public Page<ProductDTO> findAll(Pageable pageable) {
+        Page<Product> result = repository.findAll(pageable);
+        return result.map(x -> new ProductDTO(x));
     }
 
 }
